d08a64dd0f4bcedc3fe562cb934fb972a8cfa8ac
0o0-ct <tj7931404@gmail.com>
2025-11-09 18:41:18 -0600

    Delete src/main/java/com/ega/cms directory

--- src/main/java/com/ega/cms/model/Publishable.java
+++
@@ -1,5 +0,0 @@
-package com.ega.cms.model;
-
-public interface Publishable {
-    void publish();
-}
--- src/main/java/com/ega/cms/model/Searchable.java
+++
@@ -1,5 +0,0 @@
-package com.ega.cms.model;
-
-public interface Searchable {
-    boolean search(String keyword);
-}
