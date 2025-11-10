4483e23ef6c31ff3139aca5341de0a5e846a4a27
0o0-ct <tj7931404@gmail.com>
2025-11-09 20:29:02 -0600

    Clean up Main.java by removing comments
    Removed commented-out code for clarity.

--- src/main/java/com/ega/cms/Main.java
+++ src/main/java/com/ega/cms/Main.java
@@ -10,16 +10,11 @@ import com.ega.cms.view.CMSView;
 
 public class Main {
     public static void main(String[] args) {
-        // Create a controller
         CMSController controller = new CMSController();
-
-        // Create users
         Administrator admin = new Administrator("admin", "password");
         Editor editor = new Editor("editor", "password");
         controller.addUser(admin);
         controller.addUser(editor);
-
-        // Editor creates and edits content
         Article article = new Article("Introduction to Java", editor.getUsername(), "This is an article about Java programming.");
         controller.createContent(article);
         editor.editContent(article, "Advanced Java Programming");
@@ -29,14 +24,9 @@ public class Main {
 
         Image image = new Image("UML Diagram", editor.getUsername(), "http://example.com/uml.png", "UML Class Diagram");
         controller.createContent(image);
-
-        // Admin publishes and deletes content
         admin.publishContent(article);
         admin.deleteContent(video);
         controller.deleteContent(video);
-
-
-        // Create a view and display contents
         CMSView view = new CMSView(controller);
         view.displayContents();
     }
